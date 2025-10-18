b9738c2c8ec8cf0a81fe374e815d2f7f2b4e8429
karrianilkumar <162177384+karrianilkumar@users.noreply.github.com>
2025-10-19 01:07:42 +0530

    Create project_based_on_stack.py

---
+++ project_based_on_stack.py
@@ -0,0 +1,26 @@
+amop ==> adavanced management operations platform 
+--------------------------------------------------------->
+ML : 
+1) youtube video content generation usig the video url : 
+2) horticultre ==>application using the dialogue flow 
+3) hr helper ==> using the postgre sql db 
+4) home applicanaces 
+**) automatic mcq generations with q and answers 
+** ask ai pro chatbot ==> using gemini api key 
+--------------------------------------------------------->
+python flask : 
+1) contact application
+--------------------------------------------------------->
+django :
+1) wearther application 
+2) personal blog application 
+3) payment integration using the razorpay
+--------------------------------------------------------->
+js : 
+college exam results cehcking  and synllus copy downloads 
+youtube content generation from video link 
+
+--------------------------------------------------------->
+reactjs : 
+1) mobile applications showing 
+2) horticulture for front end
